e426329574362bd1d5732cc5d9fdbc59aec562eb
Jeff Jakinovich <jeff.jak13@gmail.com>
2020-07-05 15:05:32 -0400

    Final version with express server fully set up

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+
+  let result = num1 + num2;
+
+  res.send("The result you requested is " + result);
+});
+
+app.listen(port, function () {
+  console.log("server started on port 3000");
+});
