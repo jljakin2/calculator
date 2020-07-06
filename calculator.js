c7aa2ca54cf7c76616958487cd610e9eaa02907e
Jeff Jakinovich <jeff.jak13@gmail.com>
2020-07-05 22:10:30 -0400

    Added BMI calc page and fixed  final server side bug

--- calculator.js
+++ calculator.js
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Get and post methods for calculator page
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -16,6 +18,21 @@ app.post("/", function (req, res) {
   let result = num1 + num2;
 
   res.send("The result you requested is " + result);
+});
+
+// Get and post methods for BMI calculator page
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+
+  let bmiResult = (weight / (height ^ 2)) * 703;
+
+  res.send("Your BMI is " + bmiResult);
 });
 
 app.listen(port, function () {
